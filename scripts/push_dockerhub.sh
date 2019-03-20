77aea73ef16baffb9d08ccce103a53669b30e011
nakamura_subaru <subaru.nakamura@drecom.co.jp>
2019-03-20 19:17:42 +0900

    Modified docker push path

--- scripts/push_dockerhub.sh
+++ scripts/push_dockerhub.sh
@@ -14,4 +14,4 @@ IMAGE_NAME=gcr.io/${PROJECT_ID}/${REPO_NAME}:${COMMIT_SHA}
 
 docker login --username=drecomdockerhub --password=${DOCKERHUB_PASSWORD}
 docker tag ${IMAGE_NAME} drecom/centos-rails:${VERSION}
-docker push drecom/centos-ruby:${VERSION}
+docker push drecom/centos-rails:${VERSION}
